a06a234895c42cbbe353bbc2c69d4b75c2405a93
Luxion009 <cokusyou@gmail.com>
2014-03-14 17:19:54 +0900

    0.01.001

---
+++ FMO_core.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        Foil Master Outputer
+# Purpose:
+#
+# Author:      Luxion009
+#
+# Created:     14/03/2014
+# Copyright:   (c) Luxion009 2014
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
